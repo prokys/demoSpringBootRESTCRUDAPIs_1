18dd94c636b8346ba34eacf931ade292c2a28db3
Prokop Ferdan <prokes1@gmail.com>
2024-07-22 11:26:21 +0200

    added findyById, save, deleteByID methods to EmployeeService and EmployeeServiceImpl

--- src/main/java/com/prokys/demoCRUD/service/EmployeeServiceImpl.java
+++ src/main/java/com/prokys/demoCRUD/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.prokys.demoCRUD.service;
 import com.prokys.demoCRUD.dao.EmployeeDAO;
 import com.prokys.demoCRUD.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -16,5 +17,20 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+    @Transactional
+    @Override
+    public void deleteByID(int theId) {
+        employeeDAO.deleteByID(theId);
     }
 }
