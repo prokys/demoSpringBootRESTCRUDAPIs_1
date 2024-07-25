d560f0c5dd04d4bd1e26337406ee01cdb1263733
Prokop Ferdan <prokes1@gmail.com>
2024-07-25 16:27:01 +0200

    added restriction to endpoints based by user role

--- src/main/java/com/prokys/demoCRUD/security/DemoSecurityConfig.java
+++ src/main/java/com/prokys/demoCRUD/security/DemoSecurityConfig.java
@@ -2,10 +2,13 @@ package com.prokys.demoCRUD.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -30,4 +33,24 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/magic-api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/magic-api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/magic-api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/magic-api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/magic-api/employees/**").hasRole("ADMIN"));
+
+        // use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cros site request forgery(CSRF)
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
 }
