e8edcc885f05de2b4571afcefde7f4ecf68554a1
Prokop Ferdan <prokes1@gmail.com>
2024-08-14 19:18:29 +0200

    added endpoint to save new Employee, added button to go back to empÅloyee list to employee-form

--- src/main/java/com/prokys/demoCRUD/controller/EmployeeController.java
+++ src/main/java/com/prokys/demoCRUD/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.prokys.demoCRUD.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -40,6 +42,15 @@ public class EmployeeController {
         theModel.addAttribute("employee", theEmployee);
 
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        //save employee
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
     }
 
 }
