8f481b2a934741dbe3deba3ade151c6deaf7280e
Prokop Ferdan <prokes1@gmail.com>
2024-08-28 20:57:36 +0200

    added @Pointcuts for controller, service, DAO and combined, added @Before to log method signatures

--- src/main/java/com/prokys/demoCRUD/aspect/DemoLoggingAspect.java
+++ src/main/java/com/prokys/demoCRUD/aspect/DemoLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.prokys.demoCRUD.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -11,5 +14,27 @@ public class DemoLoggingAspect {
 
     //setup logger
     private Logger logger = Logger.getLogger(getClass().getName());
+
+    //setup pointcut declaration
+    @Pointcut("execution(* com.prokys.demoCRUD.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    @Pointcut("execution(* com.prokys.demoCRUD.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("execution(* com.prokys.demoCRUD.dao.*.*(..))")
+    private void forDAOPackage(){}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
+    private void forAppFlow(){}
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint){
+        // display method we are calling
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("====>>>> in @Before: calling method: " + method);
+
+        // display the arguments to the method
+    }
 
 }
